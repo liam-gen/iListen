017826002619f818001f658935fc0fe9b983185a
liamgen.js <liamgen.js@proton.me>
2023-09-24 18:00:34 +0200

    v1.0.0

--- test.js
+++
@@ -1,9 +0,0 @@
-const youtubedl = require('youtube-dl-exec')
-
-youtubedl('https://www.youtube.com/watch?v=KOxC0axwLUc', {
-  dumpSingleJson: true,
-  noCheckCertificates: true,
-  noWarnings: true,
-  preferFreeFormats: true,
-  addHeader: ['referer:youtube.com', 'user-agent:googlebot']
-}).then(output => console.log(output["requested_formats"][1]["url"]))
